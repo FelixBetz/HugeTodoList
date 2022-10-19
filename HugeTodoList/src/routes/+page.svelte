9f8f71f79c06426507da504e39029806a2aba863
Felix Betz <betz.felix@web.de>
2022-10-19 13:31:22 +0200

    add bootstrap + simple push notification

--- HugeTodoList/src/routes/+page.svelte
+++ HugeTodoList/src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { Button } from 'sveltestrap/src';
+
+	function showNotification() {
+		const title = 'Test: ' + Date.now().toString();
+		new Notification(title, { body: 'test tests' });
+	}
+
+	function checkNotification() {
+		console.log(Notification.permission);
+		window.Notification.requestPermission();
+		if (Notification.permission === 'granted') {
+			showNotification();
+		} else if (Notification.permission !== 'denied') {
+			Notification.requestPermission().then((permission) => {
+				if (permission === 'granted') {
+					showNotification();
+				}
+			});
+		}
+	}
+</script>
+
+<Button color="danger" on:click={checkNotification}>send Notification</Button>
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # HugeTodoListe
+
+## docs
+
+sveltestrap: https://sveltestrap.js.org/?path=/story/components--get-started
+
+## workaround when running npm run dev
+
+add `"type": "module",` to `@popperjs/core/package.json`
