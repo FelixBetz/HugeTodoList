4689a80dc281e88c1a50aa0bb298b244d68dcc80
Felix Betz <betz.felix@web.de>
2022-10-20 10:42:28 +0200

    add navbar, move test to extra components

--- HugeTodoList/src/routes/+layout.svelte
+++ HugeTodoList/src/routes/+layout.svelte
@@ -1,7 +1,44 @@
-<script>
-	import { Styles } from 'sveltestrap';
+<script lang="ts">
+	import { Styles } from 'sveltestrap/src';
+	import {
+		Navbar,
+		NavItem,
+		Collapse,
+		NavbarToggler,
+		NavbarBrand,
+		Nav,
+		NavLink
+	} from 'sveltestrap/src';
+	let isOpen = false;
+	function handleUpdate(event: CustomEvent<boolean>) {
+		isOpen = event.detail;
+	}
+
+	interface Links {
+		label: string;
+		route: string;
+	}
+	const navItems: Links[] = [
+		{ label: 'Home', route: '/' },
+		{ label: 'notifyTest', route: '/notifyTest' },
+		{ label: 'storageTest', route: '/storageTest' }
+	];
 </script>
 
 <Styles />
+
+<Navbar color="light" light expand="md">
+	<NavbarBrand href="/">HugeTodoList</NavbarBrand>
+	<NavbarToggler on:click={() => (isOpen = !isOpen)} />
+	<Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+		<Nav class="ms-auto" navbar>
+			{#each navItems as item}
+				<NavItem>
+					<NavLink href={item.route}>{item.label}</NavLink>
+				</NavItem>
+			{/each}
+		</Nav>
+	</Collapse>
+</Navbar>
 
 <slot />
